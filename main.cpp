95f27f15cda6224ab19a59e4750984e077392f8e
jncosideout <beatya4@gator.uhd.edu>
2017-04-24 19:01:20 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,38 +1,13 @@
-#include "mywindow.h"
+#include "sims.h"
+#include "ui_sims.h"
+
 #include <QApplication>
-#include <QtSql/QtSql>
-#include <Qtsql/QSqlDatabase>
-#include <QtSql/QSqlQuery>
-#include <QSqlDriver>
-
-#include <iostream>
-using namespace std;
-
-
-
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MyWindow w;
+    SIMS w;
+
     w.show();
-    QString dbname= "mydb";
-    QSqlDatabase db= QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("DRIVER={MySQL ODBC 5.3 Unicode Driver};SERVER=LOCALHOST;Database=mydb;Uid=root;Password=root;");
-
-    if(db.open())
-    {
-
-        cout<< "Database connected"<<endl;
-        w.buildList();
-    }
-    else
-    {
-        cout << "Database connect failed"<<endl;
-        qDebug() <<"error connect" << db.lastError().text();
-    }
-
     return a.exec();
 }
-
-
